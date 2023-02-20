73fe2c106004499c1d1860c7f2f91c81b6ded2ea
Gabriel Pontel Dal Bó <gabrielpdalbo@gmail.com>
2023-02-20 20:08:41 -0300

    Route Params

--- src/server.js
+++ src/server.js
@@ -1,7 +1,20 @@
+// Importação do express
 const express = require('express')
 
+// Inicializa o express e armazena na const app
 const app = express()
 
+// Define que, utilizando o método "get", na rota "/message", o servidor responderá, através do método "response.send", "Hello World". Na callback para os métodos, é possível capturar o "request" e o "response", sempre nessa ordem. Route Params é o nome que se dá aos dois itens que aparecem na rota depois do ":", como id e user.
+app.get('/message/:id/:user', (request, response) => {
+  // Desestruturando "id" e "user" de dentro do "request.params"
+  const { id, user } = request.params
+
+  response.send(`Mensagem ID: ${id}.
+  Para o usuário: ${user}`)
+})
+
+// Define o valor de PORT como 3333
 const PORT = 3333
 
+// Faz com que o app passe a ouvir a porta (3333), e, quando iniciado, executa a arrow function retornando o log
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
