9879ad069d87c090b82b7cd639e56d4c3a9f5f44
Gabriel Pontel Dal Bó <gabrielpdalbo@gmail.com>
2023-08-02 21:11:20 -0300

    Nodemon

--- src/server.js
+++ src/server.js
@@ -2,6 +2,10 @@ const express = require('express')
 
 const app = express()
 
+app.get('/', (req, res) => {
+  res.json({ message: 'Hi' })
+})
+
 const PORT = 3333
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
