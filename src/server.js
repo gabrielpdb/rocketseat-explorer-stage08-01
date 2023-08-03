cb40464f4ef1e554b8839cdc700cd41e0c734a37
Gabriel Pontel Dal Bó <gabrielpdalbo@gmail.com>
2023-08-03 20:41:50 -0300

    Conectando com o banco de dados

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 require('express-async-errors')
 
+const database = require('./database/sqlite')
 const AppError = require('./utils/AppError')
 const express = require('express')
 const routes = require('./routes')
@@ -7,6 +8,8 @@ const routes = require('./routes')
 const app = express()
 
 app.use(express.json())
+
+database()
 
 app.use(routes)
 
