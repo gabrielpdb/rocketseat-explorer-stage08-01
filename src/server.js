414e4db35fc328f3565d5d3b140ac39175d52a40
Gabriel Pontel Dal Bó <gabrielpdalbo@gmail.com>
2023-08-02 21:31:25 -0300

    Controllers

--- src/server.js
+++ src/server.js
@@ -1,10 +1,11 @@
 const express = require('express')
+const routes = require('./routes')
 
 const app = express()
 
-app.get('/', (req, res) => {
-  res.json({ message: 'Hi' })
-})
+app.use(express.json())
+
+app.use(routes)
 
 const PORT = 3333
 
