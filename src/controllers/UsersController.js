a71821d5179892038c1fefb150190b18d362a3e7
Gabriel Pontel Dal Bó <gabrielpdalbo@gmail.com>
2023-08-05 22:53:58 -0300

    Cadastrando usuário

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,14 +1,28 @@
 const AppError = require('../utils/AppError')
 
+const sqliteConnection = require('../database/sqlite')
+
 class UsersController {
-  create(req, res) {
+  async create(req, res) {
     const { name, email, password } = req.body
 
-    if (!name) {
-      throw new AppError('Nome obrigatório')
+    const database = await sqliteConnection()
+
+    const checkUserExist = await database.get(
+      'SELECT * FROM users WHERE email = (?)',
+      [email]
+    )
+
+    if (checkUserExist) {
+      throw new AppError('Este e-mail já está em uso')
     }
 
-    return res.status(201).json({ name, email, password })
+    await database.run(
+      'INSERT INTO users (name, email, password) VALUES (?, ?, ?)',
+      [name, email, password]
+    )
+
+    return res.status(201).json()
   }
 }
 
