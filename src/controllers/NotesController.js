d8359ef0f122763a4c23f6d33e5416601eb07f4c
Gabriel Pontel Dal Bó <gabrielpdalbo@gmail.com>
2023-08-07 20:57:16 -0300

    Deletando nota

--- src/controllers/NotesController.js
+++ src/controllers/NotesController.js
@@ -37,8 +37,24 @@ class NotesController {
     const { id } = req.params
 
     const note = await knex('notes').where({ id }).first()
+    const tags = await knex('tags').where({ note_id: id }).orderBy('name')
+    const links = await knex('links')
+      .where({ note_id: id })
+      .orderBy('created_at')
 
-    return res.json(note)
+    return res.json({
+      ...note,
+      tags,
+      links
+    })
+  }
+
+  async delete(req, res) {
+    const { id } = req.params
+
+    await knex('notes').where({ id }).delete()
+
+    return res.json()
   }
 }
 
